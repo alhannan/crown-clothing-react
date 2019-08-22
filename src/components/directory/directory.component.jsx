fbc0ff74413f638579d10607e626918a00bee9e7
abdulhannan8122 <abdulhannan8122@gmail.com>
2019-08-22 18:59:43 +0500

    routing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,12 +49,10 @@ class Directory extends React.Component {
     return (
       <div className='directory-menu'>
         {
-          this.state.sections.map(({ title, id, imageUrl, size }) => 
+          this.state.sections.map(({ id, ...otherSectionProps }) => 
             <MenuItem 
               key={id}
-              title={title} 
-              image={imageUrl}
-              size={size}
+              {...otherSectionProps}
             />
           )
         }

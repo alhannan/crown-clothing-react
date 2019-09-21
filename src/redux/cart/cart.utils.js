3798d0c59b91f232d094262a61ad74169acc8371
abdulhannan8122 <abdulhannan8122@gmail.com>
2019-09-21 16:51:32 +0500

    problem solved in cart.utils function, use ? : operators instead of &&

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -5,8 +5,9 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   
   if (existingCartItem) {
     return cartItems.map(cartItem => 
-      cartItem.id === cartItemToAdd.id && 
+      cartItem.id === cartItemToAdd.id ? 
       { ...cartItem, quantity: cartItem.quantity + 1 }
+      : cartItem
     )
   }
 
